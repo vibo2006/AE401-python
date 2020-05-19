04107e500e64a3cf992b0c33f4432dc3035fb059
vibo2006 <65231440+vibo2006@users.noreply.github.com>
2020-05-19 20:35:46 +0800

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 19 19:32:47 2020
+
+@author: TONYBH
+"""
+
+score=int(input('輸入成績0-100?'))
+
+    
+    
+if score<=100:  
+    if score>90:
+        print ('A')
+    elif score>80:
+        print('B')
+    elif score>70:
+        print('C')
+    elif score>60:
+        print('D')
+    else:   
+        print('E')
+else:
+    print('請輸入0-100的成績')   
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
