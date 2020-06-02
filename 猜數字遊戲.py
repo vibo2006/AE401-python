08bd74b27780eec171b027ae4f3e8d5c5d82f5fd
vibo2006 <65231440+vibo2006@users.noreply.github.com>
2020-06-02 18:43:37 +0800

    Add files via upload

--- 猜數字遊戲.py
+++ 猜數字遊戲.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 import random
-a=random.randint(1,30)
+a=int(random.randint(1,30))
 while:
     input=print('guess')
     guess=int(b)
-    A=A+1
+    A=int(A+1)
     if A=5:
         print('game over')
         break
@@ -12,8 +12,23 @@ while:
         print('too big')
     if b<a:
         print('too small')
-
-
+    if b=a:
+        print('bingo')
+input=print('play again')
+if true:
+    while:
+    input=print('guess')
+    guess=int(b)
+    A=int(A+1)
+    if A=5:
+        print('game over')
+        break
+    if b>a:
+        print('too big')
+    if b<a:
+        print('too small')
+    if b=a:
+        print('bingo')
 
 
 
