a737f98e78a95c9bd2483239f727fa55757359bd
vibo2006 <65231440+vibo2006@users.noreply.github.com>
2020-06-02 18:41:47 +0800

    Add files via upload

---
+++ 猜數字遊戲.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+import random
+a=random.randint(1,30)
+while:
+    input=print('guess')
+    guess=int(b)
+    A=A+1
+    if A=5:
+        print('game over')
+        break
+    if b>a:
+        print('too big')
+    if b<a:
+        print('too small')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
