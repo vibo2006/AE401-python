0a36d764833b99fd9b1adb8f7b8d1c78cced9f4d
vibo2006 <65231440+vibo2006@users.noreply.github.com>
2020-06-09 18:20:15 +0800

    Add files via upload

---
+++ draw.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  9 18:14:50 2020
+
+@author: TONYBH
+"""
+
+
+import turtle
+david=turtle.Turtle()
+david.color('purple')
+x=turtle.Screen()
+x.bgcolor('yellow')
+for i in range(10):
+    david.forward(100)
+    david.left(36)
