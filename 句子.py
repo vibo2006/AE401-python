2fd088dd0c679aca4ad74f994ceff8e901c2434e
vibo2006 <65231440+vibo2006@users.noreply.github.com>
2020-06-30 18:43:15 +0800

    Add files via upload

---
+++ 句子.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+verb=[]
+sub=[]
+ob=[]
+v=input('Verb?')
+s=input('subject?')
+o=input('object?')
+verb.append(v)
+sub.append(s)
+ob.append(o)
+a=input('How many sentences do you want?')
+for i in range(int(a)):
+    print()
+
+
