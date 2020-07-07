234823f02a37024720f75e97624d34ef82ddc8b0
vibo2006 <65231440+vibo2006@users.noreply.github.com>
2020-07-07 18:43:53 +0800

    英文字典

---
+++ dictionary.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+d={}
+
+
+num=input('多少個單字?')
+a=input('英文')                                                                                                   
+b=input('中文')
+for i in range(int(num)):
+    d['a']='b'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
